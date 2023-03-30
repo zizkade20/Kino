4d4076551c5382c5493e5fd0dca70686d1488d29
Denis Žižka <zizkade20@sps-prosek.cz>
2023-03-30 15:47:03 +0200

    Přidejte soubory projektu.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int Rows = 5;
+            int Columns = 10;
+            grid.Rows = Rows;
+            grid.Columns = Columns;
+            for (int y = 0; y < Rows; y++)
+            {
+                for (int i = 0; i < Columns; i++)
+                {
+                    Button button = new Button()
+                    {
+                        Content = string.Format("{0}", i),
+                        
+                        Tag = i
+                    };
+                    button.Margin = new Thickness(0, 15, 0, 0);
+                    this.grid.Children.Add(button);
+                }
+            }
+
+
+
+        }
+
+   
+    }
+}
